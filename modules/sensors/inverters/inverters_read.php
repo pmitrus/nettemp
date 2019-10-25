970b33ac242989d23274c603e4ae9de6452d7eb9
Mariusz Musik <m@serwer-robelit.local>
2019-10-25 18:51:40 +0200

    Update inverters_read.php

--- modules/sensors/inverters/inverters_read.php
+++ modules/sensors/inverters/inverters_read.php
@@ -159,8 +159,8 @@ try {
 								$local_type = 'frequency';
 								db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 								
-								$current time = strtotime('now');
-									if ($current_time > strtotime('12:00pm') && $current_time < strtotime('00:05am')) {
+								$current_time = strtotime('now');
+									if ($current_time > strtotime('12:00am') && $current_time < strtotime('00:05am')) {
 										
 										// Day Energy - reset if midnight
 										
@@ -233,7 +233,7 @@ try {
 						$local_type = 'watt';
 						db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 						
-						$current time = strtotime('now');
+						$current_time = strtotime('now');
 									if ($current_time > strtotime('18:00pm') && $current_time < strtotime('18:55pm')) {
 										
 							//Day Energy
