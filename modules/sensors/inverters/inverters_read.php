809b387bbe7eeb61a95ba21422f5fad01ebc55d9
Mariusz Musik <m@serwer-robelit.local>
2019-10-25 19:09:41 +0200

    Update inverters_read.php

--- modules/sensors/inverters/inverters_read.php
+++ modules/sensors/inverters/inverters_read.php
@@ -159,19 +159,18 @@ try {
 								$local_type = 'frequency';
 								db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 								
-									if( $date->format( 'H') == 18 && $date->format( 'i') == 36) {
+									//if( $date->format( 'H') == 18 && $date->format( 'i') == 36) {
 										
 										// Day Energy - reset if midnight
 										
-										$local_rom = $rom."_day";
-										$local_val = 0;
-										$local_type = 'kwatt';
-										db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
-									}
+									//	$local_rom = $rom."_day";
+									//	$local_val = 0;
+										//$local_type = 'kwatt';
+										//db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
+									//}
 								}	
-							}
-
-				}		
+				}
+		
 // Zeversolar Inverters					
 				if ($inv_type == 'zeversolar'){
 					
