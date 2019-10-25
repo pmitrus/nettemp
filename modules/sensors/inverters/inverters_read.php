f00dbaa987f41f6a45047a1920a0478aef0f1aa0
Mariusz Musik <m@serwer-robelit.local>
2019-10-25 18:22:32 +0200

    Simple integration with PV inverters - Fronius, Zeversolar

--- modules/sensors/inverters/inverters_read.php
+++ modules/sensors/inverters/inverters_read.php
@@ -149,7 +149,6 @@ try {
 								$local_type = 'amps';
 								db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 								//Current produced energy - PAC
-								$pac			= $reads['Body']['Data']['PAC']['Value'];
 								$local_rom = $rom."_pac";
 								$local_val = 0;
 								$local_type = 'watt';
